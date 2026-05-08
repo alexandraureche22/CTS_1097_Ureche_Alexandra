1d3bb6e6be298f9f6f0f0862aa88e5941c3a668c
Ureche Alexandra <https://github.com/alexandraureche22/licentaPawsitive.git>
2026-05-08 08:48:48 +0300

    seminar 10 adaugat

---
+++ seminar10/src/command/Main.java
@@ -0,0 +1,27 @@
+package command;
+
+public class Main {
+    static void main() {
+        Medic medic = new Medic("Alexandra");
+        Command comanda1 = new ComandaInternare("Denis",medic);
+        Command comanda2 = new ComandaTratareImediata("Rock",medic);
+        Command comanda3 = new ComandaTratareImediata("Nicholas",medic);
+        OperatorTriaj operatorTriaj  = new OperatorTriaj();
+        operatorTriaj.adaugaComanda(comanda1);
+        operatorTriaj.adaugaComanda(comanda2);
+        operatorTriaj.adaugaComanda(comanda3);
+        operatorTriaj.trimiteComanda();
+        operatorTriaj.trimiteComanda();
+        operatorTriaj.trimiteComanda();
+        operatorTriaj.trimiteComanda();
+
+        operatorTriaj.anuleazaComandaUndo();
+        operatorTriaj.anuleazaComandaUndo();
+        operatorTriaj.anuleazaComandaUndo();
+        operatorTriaj.anuleazaComandaUndo();
+        System.out.println("\n-----");
+        operatorTriaj.refacereComandaRedo();
+        operatorTriaj.refacereComandaRedo();
+
+    }
+}
